760d8f2b59a426ed350e304fe480c68b5c31df3e
MsWebGirl <webgirllisa@gmail.com>
2018-10-13 10:50:15 -0400

    Update resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
                     <div class="navbar-header">
                         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                             <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
-                        </button><a class="navbar-brand" href="#">RoboHome</a>
+                        </button><a class="navbar-brand" href="https://github.com/dbudwin/RoboHome-Web">RoboHome</a>
                     </div>
                 </div>
             </nav>
